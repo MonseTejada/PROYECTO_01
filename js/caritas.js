0e28053a240180eb5f4ffe663b6f7f53eb37050b
MonseTejada <auxsoftwaregrupoccima@gmail.com>
2022-08-29 12:38:06 -0500

    Se agrega el proyecto

---
+++ js/caritas.js
@@ -0,0 +1,79 @@
+ function cara(){
+    var cara = document.getElementById("Barra").style.width= "10%"
+    cara = document.getElementById("Barra").innerHTML = "10%"
+} 
+
+function cara(){
+    var cara = document.getElementById("Barra").style.width= "10%"
+    cara = document.getElementById("Barra").innerHTML = "10%"
+}
+
+function cara(){
+    var cara = document.getElementById("Barra").style.width= "10%"
+    cara = document.getElementById("Barra").innerHTML = "10%"
+}
+function cara(){
+    var porcentaje = "10%"
+    var cara = document.getElementById("Barra").style.width= "10%"
+    cara = document.getElementById("Barra").innerHTML = porcentaje
+}
+function cara(){
+    var porcentaje = "10%"
+    var cara = document.getElementById("Barra").style.width= "10%"
+    cara = document.getElementById("Barra").innerHTML = porcentaje
+}
+
+
+function cara1(){
+    var cara1 = document.getElementById("Barra").style.width= "20%"
+    cara1 = document.getElementById("Barra").innerHTML = "20%"
+} 
+
+function cara1(){
+    var cara1 = document.getElementById("Barra").style.width= "20%"
+    cara1 = document.getElementById("Barra").innerHTML = "20%"
+}
+
+function cara1(){
+    var cara1 = document.getElementById("Barra").style.width= "20%"
+    cara1 = document.getElementById("Barra").innerHTML = "20%"
+}
+function cara1(){
+    var porcentaje = "20%"
+    var cara1 = document.getElementById("Barra").style.width= "20%"
+    cara1 = document.getElementById("Barra").innerHTML = porcentaje
+}
+function cara1(){
+    var porcentaje = "20%"
+    var cara1 = document.getElementById("Barra").style.width= "20%"
+    cara1 = document.getElementById("Barra").innerHTML = porcentaje
+}
+
+function cara2(){
+    var cara2 = document.getElementById("Barra").style.width= "30%"
+    cara2 = document.getElementById("Barra").innerHTML = "30%"
+} 
+
+function cara2(){
+    var cara2 = document.getElementById("Barra").style.width= "30%"
+    cara2 = document.getElementById("Barra").innerHTML = "30%"
+}
+
+function cara2(){
+    var cara2 = document.getElementById("Barra").style.width= "30%"
+    cara2 = document.getElementById("Barra").innerHTML = "30%"
+}
+function cara2(){
+    var porcentaje = "30%"
+    var cara2 = document.getElementById("Barra").style.width= "30%"
+    cara2 = document.getElementById("Barra").innerHTML = porcentaje
+}
+function cara2(){
+    var porcentaje = "30%"
+    var cara2 = document.getElementById("Barra").style.width= "30%"
+    cara2 = document.getElementById("Barra").innerHTML = porcentaje
+}
+
+
+
+  
